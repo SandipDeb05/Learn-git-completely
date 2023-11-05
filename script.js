14d343874e44ba356cd8e91fbfc8a66e844611e9
SandipDeb <sandipdeb05@gmail.com>
2023-11-05 12:00:37 +0530

    files modified

---
+++ script.js
@@ -0,0 +1,10 @@
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+let meetup = JSON.parse(str, (key, value) => {
+  if (key === "date") return new Date(value);
+  return value;
+});
+
+for (let key in meetup) {
+  console.log(meetup[key]);
+}
